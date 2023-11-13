68fdfc49e83f740dd490b553fc6bc5110845d29f
Zaranix <zahrakhodabakhshian54@gmail.com>
2023-11-13 18:25:40 +0100

    Add upload queue

---
+++ index.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html dir="rtl"  lang="fa_IR">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/styles.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    
+    <title>feshar</title>
+  </head>
+  
+  <body>
+    <div id="fshr__fonts">
+      <div class="fshr__container--all fshr__border__container">
+        <div class="fshr__container--background fshr__box__contain">
+          <div class="fshr__bt__container">
+            <div class="fshr__dropdown fshr__collapsible--expanded">
+              <button
+                class="fshr__btn--upload  fshr__btn_left--smooth fshr__dropbtn"
+              >
+                <img
+                  class="fshr__collapsible__shevron fshr__icon--style-shevron"
+                  src="images/dropdown-iconW.svg"
+                  alt=""
+                />
+              </button>
+  
+              <div class="fshr__collapsible__content">
+                <ul class="fshr__list__items">
+                  <li class="fshr__list__border">
+                    <a href=""
+                      ><img
+                        class="fshr__icon--style-secondary"
+                        src="images/device.svg"
+                        alt=""
+                      />
+                      از طریق کامپیوتر
+                    </a>
+                  </li>
+              
+                
+                </ul>
+              </div>
+          </div>
+            <form name="uploader" id="image-upload-form"  method="post" enctype="multipart/form-data">
+              <label class="fshr__btn--upload fshr__btn_right--smooth" for="img-upload">
+                <img
+                  class="fshr__icon--style"
+                  src="images/pocket.svg"
+                  alt=""
+                /><span id="fshr__text">یک فایل انتخاب کنید</span></label
+              >
+              <input type="hidden" name="nonce" value="haey">
+              <input
+                class="fshr__btn--upload fshr__btn_right--smooth"
+                type="file"
+                name="image"
+                multiple
+                id="img-upload"
+                style="display: none"
+                accept="image/*"
+              />
+            </form>
+          </div>
+          <div class="fshr__msg__container">
+            <span class="fshr__msg__desk"
+              >تصاویر با پسوند های bmp , gif , png , jpg یا jpeg را اینجا بکشید
+              ...
+            </span>
+          </div>
+        </div>
+       </div>
+
+
+       
+      
+      <div style="display:none;" class="fshr__containerpanel fshr__border__container">
+        <div class="container--background">
+          <div class="box__container">
+            <div class="fshr__grid fshr__grid--1x4">
+              <div class="fshr__bt--container">
+                <button class="fshr__btn--fshr">Download JPG</button>
+              </div>
+          
+              <div class="fshr__size__container">
+                <div class="fshr__tooltip fshr__badge-item__action fshr__badge--linethrough">
+                  21.4955 KB
+                  <span class="fshr__tooltiptext fshr__tooltiptext--badge">سایز قبلی</span>
+                </div>
+                <div class="fshr__tooltip fshr__badge-item__action">
+                  154.49 KB
+                  <span class="fshr__tooltiptext fshr__tooltiptext--badge">سایز جدید</span>
+                </div>
+              </div>
+              <div class="success__container">
+                <div class="fshr__tooltip fshr__badge-item__status fshr__success" >
+                   50% کاهش
+                  <span class="fshr__tooltiptext">Done</span>
+                </div>
+              </div>
+              <div class="fshr__tooltip fshr__badge-item__name">
+                image.jpg
+                <span class="fshr__tooltiptext fshr__tooltiptext--name"> image.jpg</span>
+              </div>
+  
+             
+            </div>
+        
+          </div>
+          <div class="fshr__btn__container1">
+            <div class="fshr__btn1__container">
+              <div class="fshr__bt--container">
+                <button class="fshr__btn--fshr btn--fshr-2">
+                  <img class="fshr__ico--style" src="images/download-icon.svg" alt="" />
+                  دانلود همه
+                </button>
+              </div>
+     
+          
+            </div>
+    
+          </div>
+        </div>
+     </div>
+     <div style="display:none;" class="fshr__containerpanel fshr__border__container">
+      <div class="container--background">
+        <div class="box__container">
+          <div class="fshr__grid2 fshr__grid--1x4">
+            <div class="fshr__bt--container">
+              <button class="fshr__btn--fshr fshr__unclickableBtn">Download JPG</button>
+            </div>
+        
+     
+            <div class="fshr__container--ProgressBar">
+              <p class="fshr__text--uploading">در حال بارگذاری <img class="fshr__icon--uploading" src="images/download (9).svg" alt=""></p>
+              <div class="fshr__progress-bar__container">
+                <div class="fshr__progress-bar">
+                </div>
+              </div>
+            </div>
+            <div class="fshr__tooltip fshr__badge-item__name">
+              image.jpg
+              <span class="fshr__tooltiptext fshr__tooltiptext--name"> image.jpg</span>
+            </div>
+
+           
+          </div>
+        </div>
+        <div class="box__container">
+          <div class="fshr__grid2 fshr__grid--1x4">
+            <div class="fshr__bt--container">
+              <button class="fshr__btn--fshr fshr__unclickableBtn">Download JPG</button>
+            </div>
+     
+            <div class="fshr__container--ProgressBar">
+              <p class="fshr__text--ready">در حال آماده سازی </p>
+              <div class="fshr__progress-bar__container fshr__progress-bar__container--ready">
+                <div class="fshr__progress-bar">
+                </div>
+              </div>
+            </div>
+            <div class="fshr__tooltip fshr__badge-item__name">
+              billionphoto...882951 (1).jpg
+              <span class="fshr__tooltiptext fshr__tooltiptext--name"> billionphoto...882951 (1).jpg</span>
+            </div>
+
+           
+          </div>
+        </div>
+    
+
+     
+   
+        <div class="fshr__btn__container1">
+          <div class="fshr__btn1__container">
+            <div class="fshr__bt--container">
+              <button class="fshr__btn--fshr btn--fshr-2 fshr__unclickableBtn">
+                <img class="fshr__ico--style" src="images/download-icon.svg" alt="" />
+                دانلود همه
+              </button>
+            </div>
+   
+        
+          </div>
+  
+        </div>
+      </div>
+   </div>
+   
+   <div class="fshr__containerpanel fshr__border__container">
+   
+    <div class="fshr__bt__container fshr__bt2__container">
+     
+      <div class=" fshr__collapsible--expanded">
+        <button class="fshr__btn fshr__btn_left--smooth2 fshr__btn--gray">
+          <img
+            class="fshr__collapsible__shevron fshr__icon--style-shevron"
+            src="images/dropdown-iconG.svg"
+            alt=""
+          />
+        </button>
+
+        <div class="fshr__collapsible__content">
+          <ul class="fshr__list__items">
+            <li class="fshr__list__border2">
+              <a href=""
+                ><img
+                  class="fshr__icon--style-secondary"
+                  src="images/device.svg"
+                  alt=""
+                />
+                از طریق کامپیوتر
+              </a>
+            </li>
+         
+        
+            <li class="fshr__list__border2">
+              <a href="#"
+                ><img
+                  class="fshr__icon--style-secondary"
+                  src="images/url.svg"
+                  alt=""
+                />
+                از طریق Url
+              </a>
+            </li>
+         
+          </ul>
+        </div>
+      </div>
+     
+        <form>
+          <input
+            class="fshr__btn--fshr fshr__btn_right--smooth2 fshr__btn--gray"
+            type="file"
+            multiple
+            name="uploadfile"
+            id="img"
+            style="display: none"
+            accept="image/*"
+          />
+          <label class="fshr__btn--fshr fshr__btn_right--smooth2 fshr__btn--gray" for="img">
+            <img
+              class="fshr__icon--style"
+              src="images/pocketblue.svg"
+              alt=""
+            /><span id="fshr__text">آپلود فایل</span></label
+          >
+        </form>
+    
+    </div>
+
+    <div class="container--background2" id="upload-queue">
+ 
+      <div class="fshr__btn__container1">
+        <div class="fshr__btn1__container">
+          <div class="fshr__bt--container">
+            <button id="submit-btn" class="fshr__btn--fshr btn--fshr-2">
+              <img class="fshr__ico--style fshr__icon--animation" src="images/moveRight.svg" alt="" />
+              فشرده سازی
+            </button>
+          </div>
+ 
+      
+        </div>
+
+      </div>
+    </div>
+ </div>
+       <div style="display:none;" class="fshr__containerpanel fshr__border__container">
+
+        <button class="fshr__accordion">
+        <div>
+          <img class="fshr__icon--setting" src="images/download (8).svg" alt=""> تنظیمات پیشرفته (اختیاری) 
+        </div>
+        <img class="fshr__collapsible_shevron" src="images/dropdown-icon.svg" alt=""></button>
+        <div class="fshr__panel">
+       <div class="fshr__panel--background">
+        <span> Image Options</span>
+       </div>
+          <div class="fshr__panel--background2 fshr__grid fshr__grid--2x1">
+  
+       
+      
+              <div class="fshr__label--measure1">
+               <input  type="checkbox"   />
+               <label class="fshr__label"  >اطلاعات متا نگه داشته شود </label>
+              </div>
+         
+         
+      
+              <div class="fshr__label--measure2">
+               <input  type="checkbox"   />
+               <label class="fshr__label fshr__label--border"  >نیازی به فشرده سازی ویژه ندارم </label>
+              </div>
+           
+          </div>
+          <div class="fshr__panel--background">
+            <span>درخواست تغییرات</span>
+          </div>
+          <div class="fshr__panel--background3  fshr__grid--1x4">
+    
+            <div>
+             <span>تنظیمات با موفقیت ذخیره گردید</span>
+            </div>
+         
+        </div>
+          <button class="fshr__btn--fshr fesh__button--setting"> ذخیره تغییرات</button>
+        </div>
+       </div>
+    
+       </div>
+    
+  
+    
+
+        </div>
+        </div>
+     
+    </div>
+  </div>
+</div>
+<script src="js/main.js"></script>
+  </body>
+</html>
--- js/main.js
+++ js/main.js
@@ -1,3 +1,109 @@
+// Global data 
+const Data ={
+  // item in images: {id: int, file: file}
+  images:[],
+  // item in compressed: {id: id, name: name, url: url, size: size}
+  compressed_images : [],
+  last_id : 0 ,
+  push_image(file){
+      this.images.push({id: this.last_id++, file: file})
+    },
+  push_com_image(id, name, url, before_size, after_size){
+    this.compressed_images.push({id: id, name: name, url: url, before_size: before_size , after_size: after_size})
+  },
+  remove_image(id){
+    let i = this.images.findIndex((elem) =>{
+      elem.id == id
+    })
+    this.images.splice(i, 1)
+  },
+  }
+
+
+// Create a queue item and add it to queue-list (html element)
+function createQueueItem(id, file_name, file_size){
+  const  queue_item = document.createElement('div');
+  queue_item.setAttribute("id", `img${id}`);
+  queue_item.classList.add('box__container');
+  let  html_image_container= `
+          <div class="fshr__grid2 fshr__grid--1x4">
+            <div class=" fshr__icon--container">
+              <span class="fshr__tooltip ">
+                <span data-info=${id} class="fshr__ico--style fshr__icon--setting2 fshr__delete_btn"><svg data-v-4f42a8f0="" xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#7987a1" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle"><circle data-v-4f42a8f0="" cx="12" cy="12" r="10"/><line data-v-4f42a8f0="" x1="15" y1="9" x2="9" y2="15"/><line data-v-4f42a8f0="" x1="9" y1="9" x2="15" y2="15"/></svg></span>  
+                <span class="fshr__tooltiptext ">حذف فایل</span>
+              </span>
+            </div>
+            <div  class="fshr__size__container">
+              <div class="fshr__tooltip fshr__badge-item__action">
+              ${file_size} 
+              </div>
+            </div>
+            <div class="fshr__tooltip fshr__badge-item__name">
+              ${file_name}
+              <span class="fshr__tooltiptext fshr__tooltiptext--name"> ${file_name}</span>
+            </div>
+          </div>
+  `;
+  queue_item.innerHTML = html_image_container;
+  let uploadQueue = document.getElementById('upload-queue')
+  const delete_btn = queue_item.getElementsByClassName('fshr__delete_btn')[0];
+  delete_btn.addEventListener("click", function() {
+    let id = this.getAttribute("data-info") ;
+    uploadQueue.querySelector(`#img${id}`).remove();
+    Data.remove_image(id);
+  });
+  uploadQueue.insertBefore(queue_item, uploadQueue.firstChild);
+}
+
+// Event on uploading image
+document.getElementById("img-upload").addEventListener("change", function () {
+    for (let i = 0; i < this.files.length; i++) {
+      // Push new image to images list
+      Data.push_image(this.files[i]);
+      //Create queueItem
+      createQueueItem(Data.last_id-1, this.files[i].name, this.files[i].size);
+    
+    }
+
+})
+
+
+// Submit button event
+document.getElementById("submit-btn").addEventListener("click", () =>{
+
+for (let i = 0 ; i< Data.images.length; i++){
+let id = Data.images[i].id
+let name = Data.images[i].file.name
+let formData= new FormData();
+formData.append("image", Data.images[i].file);
+formData.append("nonce", "9660d3def1");
+$.ajax({
+  url: "https://feshar.co/v2/wp-json/rac/v2/feshar",
+  type: "POST",
+  data: formData,
+  success: function (msg) {
+    console.log(msg);
+    // Remove the image from images
+    Data.remove_image(id)
+    // Push the image to compressed_images
+    Data.push_com_image(id,name,msg.compressed_image_url, msg.before_size, msg.after_size)
+    console.log(Data.compressed_images)
+  },
+  error: function (xhr, status, errorThrown) {
+      var errorData = $.parseJSON(xhr.responseText);
+      console.log(errorData.data)
+      //$("#ma").text(errorData.data);
+  },
+  cache: false,
+  contentType: false,
+  processData: false
+});
+}
+
+})
+
+
+// Styling stuff
 var acc = document.getElementsByClassName("fshr__accordion");
 var i;
 
